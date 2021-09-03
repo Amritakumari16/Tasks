9b87f40f3789db5a7c76eb82cd6ce089462b6432
Amrita kumari <amritakumari.16022000@gmail.com>
2021-09-03 18:19:47 +0530

    added end point

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,33 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Observable, of, from } from 'rxjs';
 import { tap } from 'rxjs/operators';
-
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from '../environments/environment';
 
 @Injectable()
 export class AppService {
-
-    constructor() {
+    AUTH_ENDPOINT = 'http://127.0.0.1:8000/login';
+    
+    isCorrect = false;
+    constructor(private httpClient: HttpClient) {
     }
 
     isLoggedIn() {
         return localStorage.getItem('currentUser') ? true : false;
     }
 
-    login(email: string, password: string): Observable<boolean> {
+    login(username: string, password: string): Observable<boolean> {
 
-        // if email is valid (matches regex) and password length > 0
-        let isCorrect = false;
-        if (email === "demo@financepeer.com" && password === "financepeer@123") {
-            let user = {
-                'email': email,
-                'password': password
-            }
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            isCorrect = true;
-        }
-        return of(isCorrect);
+        // if username is valid (matches regex) and password length > 0
 
+        let payload = { username, password };
+
+        return this.httpClient.post(this.AUTH_ENDPOINT, payload)
+            .pipe(
+                map(res => this.handleResponse(res))
+            );
+        // if (username === "demo@financepeer.com" && password === "financepeer@123") {
+        //     let user = {
+        //         'username': username,
+        //         'password': password
+        //     }
+        //     localStorage.setItem('currentUser', JSON.stringify(user));
+        //     this.isCorrect = true;
+        // }
+        // return of(this.isCorrect);
+
+    }
+    private handleResponse(response: any) {
+        let { name, username } = response;
+        return true;
     }
 
     logout() {
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder,FormGroup, FormControl, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AppService } from '../app.service';
-const EMAIL_REGEX =  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+// const EMAIL_REGEX =  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -14,13 +14,13 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: ['', Validators.compose([Validators.required, Validators.pattern(EMAIL_REGEX)])],
+      username: ['', Validators.compose([Validators.required, ])],
       password: ['', Validators.compose([Validators.required])],
       remember: [true]
     })
   }
 login(){
-  this.appService.login(this.loginForm.controls.email.value, this.loginForm.controls.password.value)
+  this.appService.login(this.loginForm.controls.username.value, this.loginForm.controls.password.value)
   .subscribe(
       matched => {
         if (matched) {
